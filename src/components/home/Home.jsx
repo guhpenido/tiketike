440594f6ab414b89569702109ce903d182046a13
Gustavo Marcelo Penido Pereira <82380592+guhpenido@users.noreply.github.com>
2023-10-27 00:05:30 -0300

    Correção erro de CSS e bug

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,13 +6,14 @@ import ProdutoDisplay from "../produto/Produto";
 import Header from "../header/Header";
 import Carrinho from "../carrinho/Carrinho";
 import Detalhe from "../detalhes/Detalhe";
+import Select from "react-select";
 
 function Home() {
   const [carrinhoItems, setCarrinhoItems] = useState([]);
   const [detalheVisivel, setDetalheVisivel] = useState(false);
   const [produtoSelecionado, setProdutoSelecionado] = useState(null);
   const [categoriaSelecionada, setCategoriaSelecionada] = useState("Todas");
-  
+  const [faixaPrecoSelecionada, setFaixaPrecoSelecionada] = useState("Todas");
   const adicionarAoCarrinho = (produto) => {
     setCarrinhoItems([...carrinhoItems, produto]);
     alert("Produto adicionado com sucesso!");
@@ -36,36 +37,74 @@ function Home() {
   const atualizarCategoriaSelecionada = (event) => {
     setCategoriaSelecionada(event.target.value);
   };
+  const categorizarPorFaixaPreco = (valor) => {
+    if (valor >= 0 && valor < 50) {
+      return "0-50";
+    } else if (valor >= 50 && valor < 100) {
+      return "50-100";
+    } else if (valor >= 100 && valor < 150) {
+      return "100-150";
+    } else {
+      return "150+";
+    }
+  };
+  const atualizarFaixaPrecoSelecionada = (event) => {
+    setFaixaPrecoSelecionada(event.target.value);
+  };
   const produtosFiltrados = productsData.eventos.filter((produto) => {
-    return (
-      categoriaSelecionada === "Todas" || produto.tipo === categoriaSelecionada
-    );
+    const preco = parseFloat(produto.valor);
+
+    const categoriaFiltrada =
+      categoriaSelecionada === "Todas" || produto.tipo === categoriaSelecionada;
+
+    const faixaPrecoFiltrada =
+      faixaPrecoSelecionada === "Todas" ||
+      (faixaPrecoSelecionada === "0-50" && preco >= 0 && preco <= 50) ||
+      (faixaPrecoSelecionada === "50-100" && preco > 50 && preco <= 100) ||
+      (faixaPrecoSelecionada === "100-150" && preco > 100 && preco <= 150) ||
+      (faixaPrecoSelecionada === "150+" && preco > 150);
+
+    return categoriaFiltrada && faixaPrecoFiltrada;
   });
+
   return (
     <>
-      <div className="filtro">
-        <label htmlFor="categoriaSelect">Filtrar por Categoria:</label>
-        <select
-          id="categoriaSelect"
-          value={categoriaSelecionada}
-          onChange={atualizarCategoriaSelecionada}
-        >
-          <option value="Todas">Todas</option>
-          {getUniqueCategories(productsData.eventos).map((categoria) => (
-            <option key={categoria} value={categoria}>
-              {categoria}
-            </option>
-          ))}
-        </select>
-      </div>
       <div className="home-container">
         <Header toggleCarrinho={toggleCarrinho} />
         <div className="home-main">
           <div className="home-main-filtro"></div>
-          <div className="filtro"></div>
+          <div className="filtro">
+            <label htmlFor="categoriaSelect">Filtrar por Categoria:</label>
+            <select
+              id="categoriaSelect"
+              value={categoriaSelecionada}
+              onChange={atualizarCategoriaSelecionada}
+            >
+              <option value="Todas">Todas</option>
+              {getUniqueCategories(productsData.eventos).map((categoria) => (
+                <option key={categoria} value={categoria}>
+                  {categoria}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="faixaPrecoSelect">
+              Filtrar por Faixa de Preço:
+            </label>
+            <select
+              id="faixaPrecoSelect"
+              value={faixaPrecoSelecionada}
+              onChange={(e) => setFaixaPrecoSelecionada(e.target.value)}
+            >
+              <option value="Todas">Todas</option>
+              <option value="0-50">0 a 50</option>
+              <option value="50-100">50 a 100</option>
+              <option value="100-150">100 a 150</option>
+              <option value="150+">150+</option>
+            </select>
+          </div>
           <div className="main">
             <div className="home-main-produtos">
-            {produtosFiltrados.map((product) => (
+              {produtosFiltrados.map((product) => (
                 <ProdutoDisplay
                   key={product.id} // Adicione uma chave única usando o ID do produto
                   id={product.id}
